133abbe82e7ae6dbef3f2bbe6956b6faf4d42dfe
dfantasy2357-sys <dfantasy2357@gmail.com>
2026-06-15 10:52:26 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 window.DICE_MATH_FIREBASE_CONFIG = {
   enabled: true,
   config: {
-    apiKey: "AIzaSyDtKTxk36R8sSVFOQm1IgkivoX-Ozt-Bws",
-    authDomain: "operation-dice.firebaseapp.com",
-    databaseURL: "https://operation-dice-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "operation-dice",
-    storageBucket: "operation-dice.firebasestorage.app",
-    messagingSenderId: "991258022412",
-    appId: "1:991258022412:web:d62501ef8f289debf21429",
+    apiKey: "AIzaSyDDYp7rL17a8WSdni8gv6OesWXdT9nh_5o",
+    authDomain: "operation-dice-test.firebaseapp.com",
+    databaseURL: "https://operation-dice-test-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "operation-dice-test",
+    storageBucket: "operation-dice-test.firebasestorage.app",
+    messagingSenderId: "85463881130",
+    appId: "1:85463881130:web:16afb7d068bf5848cf47d3",
   },
 };
